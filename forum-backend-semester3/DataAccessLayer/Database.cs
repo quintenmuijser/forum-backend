d740e7b16f1c890661664b03de19c41dfad750e5
quinten <the_mc_pker@outlook.com>
2020-09-26 22:05:46 +0200

    Created topic+user placeholder controller + db connection

---
+++ forum-backend-semester3/DataAccessLayer/Database.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Interfaces;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    public class Database : IDatabase
+    {
+        private readonly string connectionString = "server=localhost;user=root;database=forum;port=3308;password=";
+        public MySqlConnection GetConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+
+    }
+}
---
+++ forum-backend-semester3/DataAccessLayer/Interfaces/IDatabase.cs
@@ -0,0 +1,12 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Interfaces
+{
+    public interface IDatabase
+    {
+        MySqlConnection GetConnection();
+    }
+}
